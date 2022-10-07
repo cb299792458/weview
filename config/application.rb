5dc308dafeec0ac3e47a94844b127b01e470c23f
Brian <brianrlam@gmail.com>
2022-10-07 15:14:22 -0400

    do backend phase 0

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,12 @@ module Weview
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore,
+      key: '_weview_session',
+      same_site: :lax, 
+      secure: Rails.env.production?
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
