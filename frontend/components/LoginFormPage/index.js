9c1b8e9490e06c16d0db412aea640ed5692792c3
Brian <brianrlam@gmail.com>
2022-10-07 16:53:27 -0400

    reach interlude

---
+++ frontend/components/LoginFormPage/index.js
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import { loginUser } from "../../store/session";
+import './LoginForm.css'
+
+const LoginFormPage = () => {
+    
+    const dispatch = useDispatch();
+    const [credential, setCredential] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState([]);
+    
+    const sessionUser = useSelector(state => state.session.user);
+    if(sessionUser) return <Redirect to='/' />
+
+    const user = {credential, password}
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        return dispatch(loginUser(user))
+            .catch(async (data) => {
+                console.log(data)
+                setErrors(data.errors);
+
+                // console.log(data);
+            //     console.log('in catch')
+            //     let data;
+            //     try {
+
+            //     // .clone() essentially allows you to read the response body twice
+            //     data = await res.clone().json();
+            //     } catch {
+            //     data = await res.text(); // Will hit this case if the server is down
+            //     }
+            //     if (data?.errors) setErrors(data.errors);
+            //     else if (data) setErrors([data]);
+            //     else setErrors([res.statusText]);
+            });
+    }
+    // console.log(errors)
+    return(
+        <>
+            <h1>Log In</h1>
+            <form onSubmit={handleSubmit}>
+                <ul>
+                    {errors.map( error => {
+                        return <li key={error}>{error}</li>
+                    })}
+                </ul>
+                <label>Username or Email
+                    <input
+                        type="text"
+                        value={credential}
+                        onChange={ (e) => setCredential(e.target.value)}
+                        required>
+                    </input>
+                </label>
+                <label>Password
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={ (e) => setPassword(e.target.value)}
+                        required>
+                    </input>
+                </label>
+                <button type="submit">Log In</button>
+            </form>
+        </>
+    )
+}
+
+export default LoginFormPage;
---
+++ frontend/src/store/session.js
@@ -0,0 +1,100 @@
+import csrfFetch, { storeCSRFToken } from "./csrf"
+
+export const SET_USER = '/api/session'
+export const REMOVE_USER = '/api/session'
+export const CREATE_USER = '/api/users'
+
+export const setUser = (user) => ({
+    type: SET_USER,
+    user
+})
+
+export const removeUser = () => ({
+    type: REMOVE_USER
+})
+
+export const createUser = (user) => ({
+    type: CREATE_USER,
+    user
+})
+
+export const signUpUser = user => async dispatch => {
+    const res = await csrfFetch(CREATE_USER, {
+        method: 'POST',
+        body: JSON.stringify(user)
+    })
+    
+    let data = await res.json();
+
+    if(!data.errors){
+        storeCurrentUser(data.user);
+        dispatch(createUser(data.user));
+        dispatch(setUser(data.user));
+        return res;
+    } else {
+        throw data;
+    }
+
+}
+
+export const loginUser = user => async dispatch => {
+    const res = await csrfFetch(SET_USER, {
+        method: 'POST',
+        body: JSON.stringify(user)
+    })
+    let data = await res.json();
+    
+    if(!data.errors){
+        // console.log('res ok')
+        dispatch(setUser(data.user));
+        return res;
+    } else {
+        throw data;
+    }
+}
+
+export const logoutUser = userId => async dispatch => {
+    const res = await csrfFetch(REMOVE_USER, {
+        method: 'DELETE'
+    })
+    storeCurrentUser(null)
+    dispatch(removeUser(userId)) // don't need id
+    return res;
+}
+const storeCurrentUser = user => {
+    if (user) {
+        const data = JSON.stringify(user)
+        sessionStorage.setItem('currentUser', data)
+    } else sessionStorage.removeItem('currentUser')
+}
+
+export const restoreSession = () => async dispatch => {
+    const res = await csrfFetch(SET_USER)
+    storeCSRFToken(res)
+    const data = await res.json()
+    storeCurrentUser(data.user)
+    dispatch(setUser(data.user))
+}
+
+const sessionReducer = (state = initialState, action) => {
+    // const nextState = {...state}
+    switch(action.type) {
+        case SET_USER:
+            // nextState["user"] = action.user;
+            return { ...state, user: action.user };
+            // return nextState;
+        case REMOVE_USER:
+            // const nextState = {...state};
+            // delete nextState[action.userId];
+            // return nextState;
+            return {...state, user: null}
+        default:
+            return state
+    }
+}
+
+const initialState = {
+    user: JSON.parse(sessionStorage.getItem('currentUser'))
+}
+
+export default sessionReducer;
