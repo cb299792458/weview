ab26b469b854bb1d29c66caf5bf290644b661900
Brian <brianrlam@gmail.com>
2022-10-20 08:00:41 -0400

    make control panel except edit

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -4,6 +4,11 @@ import { Redirect } from "react-router-dom"
 import { Link } from "react-router-dom/cjs/react-router-dom.min"
 import * as sessionActions from "../../store/session";
 import ModalForm from "../ModalForm";
+import profile from '../../profile.jpg'
+import signout from '../../signout.png'
+import upload from '../../upload.png'
+import edit from '../../edit.png'
+
 
 const Navigation = () => {
 
@@ -18,12 +23,23 @@ const Navigation = () => {
 
     if (sessionUser){
         return (
-            <>
-                {/* <h2>Hello, {sessionUser.username}.</h2> */}
-                <button onClick={handleLogOut}>Logout</button>
-                <br></br>
-                <Link to="/videos/new">Upload</Link>
-            </>
+            <div id="control-panel">
+                <Link to="/videos/new">
+                    <img src={upload} alt="" title="Upload video"></img>
+                </Link>
+                <Link to={`/search/?u=${sessionUser.username}`}>
+                    <img src={profile} alt="" title="Your Videos"></img>
+                </Link>
+                <img src={edit} alt="" title="Edit user"></img>
+                <img src={signout} alt="" title="Sign out" onClick={handleLogOut}></img>
+            </div>
+
+            // <>
+            //     <h2>Hello, {sessionUser.username}.</h2>
+            //     <button onClick={handleLogOut}>Logout</button>
+            //     <br></br>
+            //     <Link to="/videos/new">Upload</Link>
+            // </>
         )
     } else {
 
