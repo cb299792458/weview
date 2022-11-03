f0a8f353c183bdd1323652f9a2d5d5d991082b42
Brian <brianrlam@gmail.com>
2022-11-03 09:37:42 -0400

    reseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -110,8 +110,8 @@ ApplicationRecord.transaction do
   
     v3 = Video.create!(
         uploader_id: 2,
-        title: 'bunny stretches to receive some kisses',
-        description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, 'Lorem ipsum dolor sit amet..', comes from a line in section 1.10.32."
+        title: 'Why Use WeView?',
+        description: "A skit I made with my classmates showing the purpose of WeView. Thanks to all the amazing actors! Check out the links to their projects in the left bar by clicking on the menu button."
     )
     
     why = URI.open("https://weview-seeds.s3.amazonaws.com/why.mp4")
@@ -119,8 +119,8 @@ ApplicationRecord.transaction do
   
     v4 = Video.create!(
         uploader_id: 4,
-        title: 'Why Use WeView?',
-        description: "A skit I made with my classmates showing the purpose of WeView. Thanks to all the amazing actors! Check out the links to their projects in the left bar by clicking on the menu button."
+        title: 'How to Play Sneak Snack Snake',
+        description: "This is a demonstration of my Vanilla Javascript game. Play here: https://cb299792458.github.io/snacke/"
     )
     
     snacke = URI.open("https://weview-seeds.s3.amazonaws.com/snacke.mp4")
